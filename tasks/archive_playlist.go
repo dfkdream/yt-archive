861feb8bb9051a3bbeb6fb9bd287bf9eecdfb026
dfkdream <dfkdream@gmail.com>
2024-09-05 00:07:54 +0900

    Add ArchivePlaylist task handler

---
+++ tasks/archive_playlist.go
@@ -0,0 +1,141 @@
+package tasks
+
+import (
+	"database/sql"
+	"encoding/json"
+	"log/slog"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+	"yt-archive/taskq"
+)
+
+const TASK_ARCHIVE_PLAYLIST = "ARCHIVE_PLAYLIST"
+
+type ArchivePlaylistHandler struct {
+	DB *sql.DB
+}
+
+func NewArchivePlaylistHandler(db *sql.DB) (ArchivePlaylistHandler, error) {
+	_, err := db.Exec("create table if not exists playlists (id text primary key, title text, description text, timestamp timestamp, owner text)")
+	if err != nil {
+		return ArchivePlaylistHandler{}, err
+	}
+
+	_, err = db.Exec("create table if not exists playlist_video (playlistId text, videoId text)")
+	if err != nil {
+		return ArchivePlaylistHandler{}, err
+	}
+
+	return ArchivePlaylistHandler{DB: db}, nil
+}
+
+type playlistMetadata struct {
+	ID          string `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Owner       string `json:"uploader_id"`
+	Timestamp   string `json:"modified_date"`
+}
+
+func (a ArchivePlaylistHandler) Handler(task *taskq.Task) error {
+	var playlistID string
+	err := json.Unmarshal(task.Payload, &playlistID)
+	if err != nil {
+		return err
+	}
+
+	tempDir, err := os.MkdirTemp("", playlistID+"_*")
+	if err != nil {
+		return err
+	}
+	defer os.RemoveAll(tempDir)
+
+	err = Exec("yt-dlp", "--write-info-json", "--skip-download", "-o", "%(id)s.%(ext)s", "--paths", tempDir, playlistID)
+	if err != nil {
+		return err
+	}
+
+	files, err := os.ReadDir(tempDir)
+	if err != nil {
+		return err
+	}
+
+	videos := make([]string, 0)
+
+	for _, f := range files {
+		if !strings.HasSuffix(f.Name(), ".info.json") {
+			continue
+		}
+
+		if f.Name() == playlistID+".info.json" {
+			continue
+		}
+
+		videos = append(videos, strings.TrimSuffix(f.Name(), ".info.json"))
+	}
+
+	slog.Info("extracted playlist", "videos", videos)
+
+	f, err := os.Open(filepath.Join(tempDir, playlistID+".info.json"))
+	if err != nil {
+		return err
+	}
+
+	var metadata playlistMetadata
+	err = json.NewDecoder(f).Decode(&metadata)
+	if err != nil {
+		return err
+	}
+
+	timestamp, err := time.ParseInLocation("20060102", metadata.Timestamp, time.Local)
+	if err != nil {
+		return err
+	}
+
+	for _, videoID := range videos {
+		payload, err := json.Marshal(videoID)
+		if err != nil {
+			return err
+		}
+
+		task, err := taskq.NewTask(50, TASK_ARCHIVE_VIDEO, playlistID+" - "+videoID, payload)
+		if err != nil {
+			return err
+		}
+
+		err = taskq.Enqueue(task)
+		if err != nil {
+			return err
+		}
+	}
+
+	tx, err := a.DB.Begin()
+	if err != nil {
+		return err
+	}
+
+	_, err = tx.Exec("insert into playlists (id, title, description, timestamp, owner) values (?, ?, ?, ?, ?)",
+		metadata.ID, metadata.Title, metadata.Description, timestamp, metadata.Owner)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	for _, videoID := range videos {
+		_, err = tx.Exec("insert into playlist_video (playlistId, videoId) values (?, ?)",
+			playlistID, videoID)
+		if err != nil {
+			tx.Rollback()
+			return err
+		}
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
