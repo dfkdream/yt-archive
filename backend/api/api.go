46d0c4914edf87afc4802caaf909a41d42f169a2
dfkdream <dfkdream@gmail.com>
2024-09-07 09:38:49 +0900

    Add video APIs

--- backend/api/api.go
+++ backend/api/api.go
@@ -1,25 +1,42 @@
 package api
 
 import (
+	"database/sql"
 	"net/http"
 	"os"
+
+	"github.com/gorilla/mux"
 )
 
-func New() *http.ServeMux {
-	mux := http.NewServeMux()
+func New(db *sql.DB) http.Handler {
+	r := mux.NewRouter()
 
-	mux.Handle("/", &StaticSiteServer{
-		FS:       http.FS(os.DirFS("dist")),
-		Fallback: "fallback.html",
-	})
+	r.Path("/api/videos").
+		Methods(http.MethodGet).
+		Handler(videosHandler{DB: db})
 
-	mux.Handle("/api/videos/", http.StripPrefix("/api/videos", &FileServer{
-		FS: http.FS(os.DirFS("videos")),
-	}))
+	r.Path("/api/videos/{id}").
+		Methods(http.MethodGet).
+		Handler(videoHandler{DB: db})
 
-	mux.Handle("/api/channels/", http.StripPrefix("/api/channels", &FileServer{
-		FS: http.FS(os.DirFS("channels")),
-	}))
+	r.PathPrefix("/api/videos/").
+		Methods(http.MethodGet).
+		Handler(http.StripPrefix("/api/videos/", &FileServer{
+			FS: http.FS(os.DirFS("videos")),
+		}))
 
-	return mux
+	r.PathPrefix("/api/channels/").
+		Methods(http.MethodGet).
+		Handler(http.StripPrefix("/api/channels/", &FileServer{
+			FS: http.FS(os.DirFS("channels")),
+		}))
+
+	r.PathPrefix("/").
+		Methods(http.MethodGet).
+		Handler(&StaticSiteServer{
+			FS:       http.FS(os.DirFS("dist")),
+			Fallback: "fallback.html",
+		})
+
+	return r
 }
---
+++ backend/api/videos.go
@@ -0,0 +1,103 @@
+package api
+
+import (
+	"database/sql"
+	"encoding/json"
+	"log/slog"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+type videosHandler struct {
+	DB *sql.DB
+}
+
+type Video struct {
+	ID             string
+	Title          string
+	Description    string
+	Timestamp      time.Time
+	Duration       string
+	Owner          string
+	Thumbnail      string
+	OwnerThumbnail string
+}
+
+func (v videosHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	query := `
+	select videos.id, videos.title, videos.description, timestamp, duration, owner, videos.thumbnail, channels.thumbnail
+	from videos
+	left join channels
+	on videos.owner = channels.id
+	`
+
+	rows, err := v.DB.Query(query)
+	if err != nil {
+		slog.Error("videosHandler error", "msg", err)
+		writeError(w, http.StatusInternalServerError)
+		return
+	}
+
+	result := make([]Video, 0)
+	var video Video
+	for rows.Next() {
+		err = rows.Scan(&video.ID, &video.Title, &video.Description,
+			&video.Timestamp, &video.Duration, &video.Owner, &video.Thumbnail, &video.OwnerThumbnail)
+
+		if err != nil {
+			slog.Error("videosHandler error", "msg", err)
+			writeError(w, http.StatusInternalServerError)
+			return
+		}
+
+		result = append(result, video)
+	}
+
+	writeJson(w, result)
+}
+
+type videoHandler struct {
+	DB *sql.DB
+}
+
+func (v videoHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)["id"]
+
+	query := `
+	select videos.id, videos.title, videos.description, timestamp, duration, owner, videos.thumbnail, channels.thumbnail
+	from videos
+	left join channels
+	on videos.owner = channels.id
+	where videos.id=?
+	`
+
+	row := v.DB.QueryRow(query, id)
+
+	var video Video
+	err := row.Scan(&video.ID, &video.Title, &video.Description,
+		&video.Timestamp, &video.Duration, &video.Owner, &video.Thumbnail, &video.OwnerThumbnail)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			writeError(w, http.StatusNotFound)
+		} else {
+			slog.Error("videoHandler error", "msg", err)
+			writeError(w, http.StatusInternalServerError)
+		}
+		return
+	}
+
+	writeJson(w, video)
+}
+
+func writeJson(w http.ResponseWriter, v any) {
+	w.Header().Set("Content-Type", "application/json")
+	err := json.NewEncoder(w).Encode(v)
+	if err != nil {
+		w.Header().Del("Content-Type")
+		slog.Error("writeJson error", "msg", err)
+		writeError(w, http.StatusInternalServerError)
+	}
+}
