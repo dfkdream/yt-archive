311d214daeb05157ff8451ad9e0f418ab7ee5e9a
dfkdream <dfkdream@gmail.com>
2024-09-10 20:25:08 +0900

    Add main

---
+++ backend/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"net/http"
+	"os"
+	"yt-archive/api"
+	"yt-archive/taskq"
+	"yt-archive/tasks"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	db, err := sql.Open("sqlite3", "file:yt-archive.db?_journal_mode=WAL&_txlock=immediate")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db.SetMaxOpenConns(1)
+
+	q, err := taskq.New(db)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	taskq.SetDefaultQueue(q)
+
+	archiveVideo, err := tasks.NewArchiveVideoHandler(db)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	archivePlaylist, err := tasks.NewArchivePlaylistHandler(db)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	archiveChannelInfo, err := tasks.NewArchiveChannelInfoHandler(db)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	taskq.Handler(tasks.TaskTypeArchiveVideo, archiveVideo.Handler)
+	taskq.Handler(tasks.TaskTypeDownloadMedia, tasks.DownloadMediaHandler)
+	taskq.Handler(tasks.TaskTypeArchivePlaylist, archivePlaylist.Handler)
+	taskq.Handler(tasks.TaskTypeArchiveChannelInfo, archiveChannelInfo.Handler)
+
+	go taskq.Start()
+
+	http.Handle("/", api.New(db))
+
+	addr := os.Getenv("YT_ARCHIVE_ADDR")
+	if addr == "" {
+		addr = "0.0.0.0:80"
+	}
+
+	log.Printf("YT_ARCHIVE_ADDR: %s\n", addr)
+
+	log.Fatal(http.ListenAndServe(addr, nil))
+}
