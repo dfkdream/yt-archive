3f6a9da6ceda6d376b25a058b620d60bc1c4a9e3
dfkdream <dfkdream@gmail.com>
2024-09-16 00:12:26 +0900

    Add 2560x1440 resolution to priority list

--- backend/tasks/priorities.go
+++ backend/tasks/priorities.go
@@ -25,16 +25,18 @@ func calculateVideoPriority(f format) int {
 
 	switch {
 	case pixels <= 256*144:
-		priority += 6
+		priority += 7
 	case pixels <= 426*240:
-		priority += 5
+		priority += 6
 	case pixels <= 640*360:
-		priority += 4
+		priority += 5
 	case pixels <= 854*480:
-		priority += 3
+		priority += 4
 	case pixels <= 1280*720:
-		priority += 2
+		priority += 3
 	case pixels <= 1920*1080:
+		priority += 2
+	case pixels <= 2560*1440:
 		priority += 1
 	}
 
