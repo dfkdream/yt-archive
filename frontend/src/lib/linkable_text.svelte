95098ed55ad59d5fe373ef026d0d8fff8cee96ad
dfkdream <dfkdream@gmail.com>
2024-10-13 21:54:37 +0900

    Add renderExternalLinks property

--- frontend/src/lib/linkable_text.svelte
+++ frontend/src/lib/linkable_text.svelte
@@ -1,24 +1,28 @@
 <script lang="ts">
     import { onMount } from "svelte";
 
+    export let renderExternalLinks = false;
+
     let div: HTMLDivElement;
 
     onMount(() => {
         let text = div.innerText;
-        console.log(text);
-        text = text.replaceAll(/(@.+)/g, '<a href="/channels/$1">$1</a>');
-        text = text.replaceAll("\n", "<br>");
-        text = text.replaceAll(
-            /\b((https?|ftp):\/\/[-\w@:%_\+.~#?&//=]*[-\w@:%_\+~#?&//=])\b/g,
-            '<a href="$1">$1</a>',
-        );
 
+        if (renderExternalLinks) {
+            text = text.replaceAll(
+                /\b((https?|ftp):\/\/[-\w@:%_\+.~#?&//=]*[-\w@:%_\+~#?&//=])\b/g,
+                '<a href="$1">$1</a>',
+            );
+        }
+
+        text = text.replaceAll(/(@.+)/g, '<a href="/channels/$1">$1</a>');
         const matches = text.matchAll(/\b([0-9]{1,2}):([0-5][0-9])\b/g);
         for (const m of matches) {
-            console.log(m);
             const sec = parseInt(m[1]) * 60 + parseInt(m[2]);
             text = text.replace(m[0], `<a href="?t=${sec}">${m[0]}</a>`);
         }
+
+        text = text.replaceAll("\n", "<br>");
 
         div.innerHTML = text;
     });
