30991bae236a418e92eeacfad0a0429f9f7fdc2e
dfkdream <dfkdream@gmail.com>
2024-10-24 00:25:12 +0900

    Remove redundant workbox options

--- frontend/src/service-worker.js
+++ frontend/src/service-worker.js
@@ -1,7 +1,5 @@
 import { precacheAndRoute } from "workbox-precaching";
 
 precacheAndRoute(self.__WB_MANIFEST, {
-    directoryIndex: "index.html",
-    cleanURLs: true,
     ignoreURLParametersMatching: [/.*/],
 });
