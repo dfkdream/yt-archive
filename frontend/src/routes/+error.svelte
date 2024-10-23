23715468bd227eeda2a972d38e54949003c606ab
dfkdream <dfkdream@gmail.com>
2024-10-23 18:40:47 +0900

    Add error page

---
+++ frontend/src/routes/+error.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import { page } from "$app/stores";
+    import Navbar from "$lib/navbar.svelte";
+</script>
+
+<Navbar />
+
+<h1 class="font-serif text-9xl text-center mt-4">{$page.status}</h1>
+<pre class="text-center">{$page.error?.message}</pre>
